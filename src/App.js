c5382c76646f28327e24847778634d1121ec110f
Carla Fonseca <carlafb11@gmail.com>
2022-04-27 00:20:41 -0600

    creacion de componente navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 // import NavBar from "./components/NavBar"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Main from "./components/Main"
-import Release from "./components/Release"
-import Popular from "./components/Popular"
-import Search from "./components/Search"
+import NavBar from "./components/NavBar"
+// import Main from "./components/Main"
+// import Release from "./components/Release"
+// import Popular from "./components/Popular"
+// import Search from "./components/Search"
 
 const App = () => {
-
   return (
     <div>
-      {/* <NavBar /> */}
-      <BrowserRouter>
-        <Routes>
-          <Route path="/main" element={<Main />} />
-          <Route path="/release" element={<Release />} />
-          <Route path="/popular" element={<Popular />} />
-          <Route path="/search" element={<Search />} />
-        </Routes>
-      </BrowserRouter>
+      <NavBar />
     </div>
   )
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,22 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Main from "./components/Main"
-import Release from "./components/Release"
-import Popular from "./components/Popular"
-import Search from "./components/Search"
+import Main from "./Main"
+import Release from "./Release"
+import Popular from "./Popular"
+import Search from "./Search"
 
 const NavBar = () => {
 
   return (
     <div>
       <BrowserRouter>
+        <nav>
+          <ul>
+            <li>Main</li>
+            <li>Release</li>
+            <li>Popular</li>
+            <li>Search</li>
+          </ul>
+        </nav>
         <Routes>
           <Route path="/main" element={<Main />} />
           <Route path="/release" element={<Release />} />
