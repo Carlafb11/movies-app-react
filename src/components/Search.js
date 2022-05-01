6f4601cf0867f8b6bf9db43e028610e0a9ee4184
Carla Fonseca <carlafb11@gmail.com>
2022-05-01 02:29:49 -0600

    movie page, latest movies, populars, and search working

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Grid from '@mui/material/Grid';
+import TextField from '@mui/material/TextField';
+import InputAdornment from '@mui/material/InputAdornment';
+import SearchIcon from '@mui/icons-material/Search'
+import Navbar from '../components/NavBar'
+import MovieDisplay from '../components/MovieDisplay';
+import '../styles/search.scss';
 
 const Search = () => {
+  const [results, setResults] = useState([])
+  const handleSearch = (e) => {
+    const query = e.target.value;
+    if (query.length > 0) {
+      fetch(`https://api.themoviedb.org/3/search/movie?api_key=43f1f0b78ebb70caf0b2b68f60949a2b&language=en-US&query=${query}&page=1`)
+        .then(res=> res.json())
+        .then(data => setResults(data.results));
+    }
+  }
+
   return (
-    <div>
-      <h1>Search</h1>
+    <div className='search-container'>
+      <Navbar />
+      <Grid container justifyContent='center' marginTop={4}>
+        <TextField
+          id="search-bar"
+          label="Search for your movie"
+          className="search-input"
+          onChange={(e) => handleSearch(e)}
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <SearchIcon />
+              </InputAdornment>
+            ),
+          }}
+          variant="standard"
+        />
+      </Grid>
+
+      <Grid container justifyContent='center' spacing={4} marginTop={4}>
+        {results.map((movie) => (
+          <Grid item>
+            <MovieDisplay
+              id={movie.id}
+              image={movie.poster_path}
+              title={movie.title}
+            />
+          </Grid>
+        ))}
+      </Grid>
     </div>
   )
 }
