d45bcccf9210bd28a49c6ed1a881ae6fff4a27ce
Carlafb11 <39042488+Carlafb11@users.noreply.github.com>
2022-04-30 22:14:10 -0600

    Navbar component - WIP (#1)
    * creacion de componente navbar
    
    * Navbar MUI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-// import NavBar from "./components/NavBar"
+import React from 'react'
+
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Main from "./components/Main"
-import Release from "./components/Release"
-import Popular from "./components/Popular"
-import Search from "./components/Search"
+import NavBar from "./components/NavBar"
+
 
 const App = () => {
-
   return (
     <div>
-      {/* <NavBar /> */}
-      <BrowserRouter>
-        <Routes>
-          <Route path="/main" element={<Main />} />
-          <Route path="/release" element={<Release />} />
-          <Route path="/popular" element={<Popular />} />
-          <Route path="/search" element={<Search />} />
-        </Routes>
-      </BrowserRouter>
+      <NavBar />
     </div>
   )
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,10 @@
+import React from 'react'
+import Navbar from './NavBar'
+
 const Main = () => {
   return (
     <div>
+      <Navbar/>
       <h1>Main</h1>
     </div>
   )
