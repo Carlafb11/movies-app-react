6c51f6078eddeff27589176378a2d687a3c1faf3
Carlafb11 <39042488+Carlafb11@users.noreply.github.com>
2022-05-02 19:56:47 -0600

    fixing homepage slier (#15)

--- src/components/ErrorNotFound.js
+++ src/components/ErrorNotFound.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import '../styles/error-page.scss'
 
 const ErrorNotFound = () => {
   return (
-    <h1>ERROR 404!</h1>
+    <div className="page-container">
+      <h1>/not-found</h1>
+      <h2>The page you are looking for, does not exist.</h2>
+    </div>
   )
 }
 
